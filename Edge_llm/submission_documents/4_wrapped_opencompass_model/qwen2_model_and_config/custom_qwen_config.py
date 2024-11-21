5f064e4f34e39dea74498ac67003cb02d7e58a57
jordanqi <159717173+jordanqi@users.noreply.github.com>
2024-11-20 22:32:31 -0600

    Update custom_qwen_config.py

--- Edge_llm/submission_documents/4_wrapped_opencompass_model/qwen2_model_and_config/custom_qwen_config.py
+++ Edge_llm/submission_documents/4_wrapped_opencompass_model/qwen2_model_and_config/custom_qwen_config.py
@@ -4,7 +4,7 @@ models = [
     dict(
         type=CustomQwen,
         abbr='qwen-7b-hf',
-        path='path to the model',
+        path='path to the model.bin folder',
         max_out_len=1024,
         batch_size=8,
         run_cfg=dict(num_gpus=1),
